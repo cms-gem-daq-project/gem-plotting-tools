3a42ade39f3e8555c226e4e712bec4ce30e80c09
Cameron Bravo <bravo@srv-s2g18-34-01.cms>
2017-06-19 13:08:20 +0200

    Debugging to allow any latency range to work

--- latency/LatencyScanData.py
+++ latency/LatencyScanData.py
@@ -34,13 +34,13 @@ errY= array( 'f' )
 errX=array('f')
 
 for vfat in range(24):
-    Nhits_hs[vfat] = r.TH1D("Nhits%i_h"%vfat,"Nhits%i_h"%vfat,61,139.5,200.5)
-    Nev_hs[vfat] = r.TH1D("Nev%i_h"%vfat,"Nev%i_h"%vfat,61,139.5,200.5)
+    Nhits_hs[vfat] = r.TH1D("Nhits%i_h"%vfat,"Nhits%i_h"%vfat,251,-0.5,250.5)
+    Nev_hs[vfat] = r.TH1D("Nev%i_h"%vfat,"Nev%i_h"%vfat,251,-0.5,250.5)
     pass
 
 for evt in t:
-    Nhits_hs[int(evt.vfatN)].AddBinContent(evt.lat - 139,evt.Nhits)
-    Nev_hs[int(evt.vfatN)].AddBinContent(evt.lat - 139,evt.Nev)
+    Nhits_hs[int(evt.vfatN)].AddBinContent(evt.lat + 1,evt.Nhits)
+    Nev_hs[int(evt.vfatN)].AddBinContent(evt.lat + 1,evt.Nev)
     pass
 
 canv = r.TCanvas("canv","canv",1000,1000)
@@ -55,7 +55,7 @@ for vfat in range(0,24):
     vfats.append(vfat)
     lat_ga[vfat].Draw("AP")
     fit_f[vfat].SetParameter(0, 0.0005)
-    lat_ga[vfat].Fit(fit_f[vfat])
+    lat_ga[vfat].Fit(fit_f[vfat],"Q")
     parameters = fit_f[vfat].GetParameters()
     par.append(parameters[0])
     errY.append(fit_f[vfat].GetParError(0))
@@ -65,9 +65,9 @@ for vfat in range(0,24):
 
 tgr= r.TGraphErrors(24,vfats,par,errX,errY)
 tgr.SetName("Average_Nhits_per_Vfat")
-canv1 = r.TCanvas("canv1","canv1")
-canv1.cd()
-canv1.SetGrid()
+tgr.SetMarkerStyle(2)
+tgr.SetMarkerColor(r.kRed+1)
+tgr.SetLineColor(r.kRed+1)
 tgr.GetXaxis().SetLimits(0,24)
 tgr.GetXaxis().SetTitle("VFAT Position")
 tgr.GetYaxis().SetTitle("Average Nhits")
@@ -78,7 +78,7 @@ tgr.Draw("AP")
 tgr.SetMarkerStyle(20)
 tgr.SetMarkerSize(0.7)
 tgr.SetTitle("Average Nhits per Vfat")
-canv1.SaveAs("Average_Nhits_per_vfat.png")
+canv.SaveAs("Average_Nhits_per_vfat.png")
 tgr.Write()
 
 
