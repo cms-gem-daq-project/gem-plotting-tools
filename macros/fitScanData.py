3cc7dca5e056b6ff75cd76638e410ed04a1b6bd4
Louis Moureaux <m_louis30@yahoo.com>
2017-07-25 16:23:37 +0200

    Make scan* variables members in ScanDataFitter

--- macros/fitScanData.py
+++ macros/fitScanData.py
@@ -1,33 +1,37 @@
 
 class ScanDataFitter:
-    def fit(self, treeFile):
+    def __init__(self):
         import ROOT as r
         import numpy as np
         from gempython.utils.nesteddict import nesteddict as ndict
+        r.gStyle.SetOptStat(0)
+
+        self.scanHistos = ndict()
+        self.scanCount  = ndict()
+        self.scanFits   = ndict()
+
+        for vfat in range(0,24):
+            self.scanFits[0][vfat] = np.zeros(128)
+            self.scanFits[1][vfat] = np.zeros(128)
+            self.scanFits[2][vfat] = np.zeros(128)
+            self.scanFits[3][vfat] = np.zeros(128)
+            self.scanFits[4][vfat] = np.zeros(128)
+            self.scanFits[5][vfat] = np.zeros(128)
+            for ch in range(0,128):
+                self.scanHistos[vfat][ch] = r.TH1D('scurve_%i_%i_h'%(vfat,ch),'scurve_%i_%i_h'%(vfat,ch),254,0.5,254.5)
+                self.scanCount[vfat][ch] = 0
+
+    def fit(self, treeFile):
+        import ROOT as r
         r.gROOT.SetBatch(True)
         r.gStyle.SetOptStat(0)
 
         inF = r.TFile(treeFile)
 
-        scanHistos = ndict()
-        scanCount  = ndict()
-        scanFits   = ndict()
-
-        for vfat in range(0,24):
-            scanFits[0][vfat] = np.zeros(128)
-            scanFits[1][vfat] = np.zeros(128)
-            scanFits[2][vfat] = np.zeros(128)
-            scanFits[3][vfat] = np.zeros(128)
-            scanFits[4][vfat] = np.zeros(128)
-            scanFits[5][vfat] = np.zeros(128)
-            for ch in range(0,128):
-                scanHistos[vfat][ch] = r.TH1D('scurve_%i_%i_h'%(vfat,ch),'scurve_%i_%i_h'%(vfat,ch),254,0.5,254.5)
-                scanCount[vfat][ch] = 0
-
         for event in inF.scurveTree :
-            scanHistos[event.vfatN][event.vfatCH].Fill(event.vcal,event.Nhits)
+            self.scanHistos[event.vfatN][event.vfatCH].Fill(event.vcal,event.Nhits)
             if(event.vcal > 250):
-                scanCount[event.vfatN][event.vfatCH] += event.Nhits
+                self.scanCount[event.vfatN][event.vfatCH] += event.Nhits
                 pass
             pass
 
@@ -53,7 +57,7 @@ class ScanDataFitter:
                     fitTF1.SetParLimits(0, 0.01, 300.0)
                     fitTF1.SetParLimits(1, 0.0, 100.0)
                     fitTF1.SetParLimits(2, 0.0, 300.0)
-                    fitResult = scanHistos[vfat][ch].Fit('myERF','SQ')
+                    fitResult = self.scanHistos[vfat][ch].Fit('myERF','SQ')
                     #fitStatus = fitResult.Status()
                     fitChi2 = fitTF1.GetChisquare()
                     fitNDF = fitTF1.GetNDF()
@@ -64,25 +68,25 @@ class ScanDataFitter:
                         chi2_iter += 1
                         pass
                     if (fitChi2 < MinChi2Temp and fitChi2 > 0.0):
-                        scanFits[0][vfat][ch] = fitTF1.GetParameter(0)
-                        scanFits[1][vfat][ch] = fitTF1.GetParameter(1)
-                        scanFits[2][vfat][ch] = fitTF1.GetParameter(2)
-                        scanFits[3][vfat][ch] = fitChi2
-                        scanFits[4][vfat][ch] = scanCount[vfat][ch]
-                        scanFits[5][vfat][ch] = fitNDF
+                        self.scanFits[0][vfat][ch] = fitTF1.GetParameter(0)
+                        self.scanFits[1][vfat][ch] = fitTF1.GetParameter(1)
+                        self.scanFits[2][vfat][ch] = fitTF1.GetParameter(2)
+                        self.scanFits[3][vfat][ch] = fitChi2
+                        self.scanFits[4][vfat][ch] = self.scanCount[vfat][ch]
+                        self.scanFits[5][vfat][ch] = fitNDF
                         MinChi2Temp = fitChi2
                         pass
                     if (fitTF1.GetParameter(0) == 8+stepN*8):
-                        scanFits[0][vfat][ch] = -999.9
-                        scanFits[1][vfat][ch] = -999.9
-                        scanFits[2][vfat][ch] = -999.9
-                        scanFits[3][vfat][ch] = -999999999.9
-                        scanFits[4][vfat][ch] = scanCount[vfat][ch]
-                        scanFits[5][vfat][ch] = -999999
+                        self.scanFits[0][vfat][ch] = -999.9
+                        self.scanFits[1][vfat][ch] = -999.9
+                        self.scanFits[2][vfat][ch] = -999.9
+                        self.scanFits[3][vfat][ch] = -999999999.9
+                        self.scanFits[4][vfat][ch] = self.scanCount[vfat][ch]
+                        self.scanFits[5][vfat][ch] = -999999
                         MinChi2Temp = fitChi2
                         pass
                     if (MinChi2Temp < 50): break
                     pass
                 pass
             pass
-        return scanFits
+        return self.scanFits
