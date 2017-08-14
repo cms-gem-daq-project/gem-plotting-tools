5ca286c2351d67d97443434a19fd3a3b0c14ff25
Brian Dorney <brian.l.dorney@cern.ch>
2017-08-14 13:32:05 +0200

    adding __init__.py files to repo and making dir structure more legible

---
+++ mapping/buildMapFiles.py
@@ -0,0 +1,19 @@
+import os
+from mapping.channelMaps import *
+from mapping.PanChannelMaps import *
+chamberType = ['long','short']
+  
+mapPath  = "%s/mapping"%(os.getenv('GEM_PLOTTING_PROJECT'))
+
+for cT in chamberType:
+    outF = open('%s/%sChannelMap.txt'%(mapPath,cT),'w')
+    outF.write('vfat/I:strip/I:channel/I:PanPin/I\n')
+    for vfat in range(0,24):
+        for strip in range(0,128):
+            channel = stripToChannel(cT,vfat,strip)
+            panpin = StripToPan(cT, vfat, strip)
+            outF.write('%i\t%i\t%i\t%i\n'%(vfat,strip,channel+1,panpin))
+            pass
+        pass
+    outF.close()
+    pass
