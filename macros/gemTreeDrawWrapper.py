eada79dc52b757b20bb8833c689db5c1348e68ad
Brian Dorney <brian.l.dorney@cern.ch>
2018-03-15 11:04:41 +0100

    gemTreeDrawWrapper.py will now use parseListOfScanDatesFile() to get input info

--- macros/gemTreeDrawWrapper.py
+++ macros/gemTreeDrawWrapper.py
@@ -147,14 +147,9 @@ if __name__ == '__main__':
         exit(os.EX_USAGE)
         pass
     
-    # Check input file
-    try:
-        fileScanDates = open(options.filename, 'r') #tab '\t' delimited file, first line column headings, subsequent lines data: cName\tscandate\tindepvar
-    except Exception as e:
-        print '%s does not seem to exist'%(options.filename)
-        print e
-        exit(os.EX_NOINPUT)
-        pass
+    # Get info from input file
+    parsedTuple = parseListOfScanDatesFile(options.filename, listDataPtTuples)
+    listChamberAndScanDate = parsedTuple[0]
     
     # Make output TFile
     listParsedExpress = options.treeExpress.split(":")
@@ -170,25 +165,7 @@ if __name__ == '__main__':
     
     # Loop Over inputs
     listPlots = []
-    for i,line in enumerate(fileScanDates):
-        if line[0] == "#":
-            continue
-
-        # On 1st iteration get independent variable name
-        if i == 0:
-            continue
-        
-        # Split the line
-        line = line.strip('\n')
-        chamberAndScanDatePair = line.rsplit('\t') #chamber name, scandate
-        if len(chamberAndScanDatePair) != 2:
-            print "Input format incorrect"
-            print "I was expecting a tab-delimited file with each line having 2 entries"
-            print "But I received:"
-            print "\t%s"%(line)
-            print "Exiting"
-            exit(os.EX_USAGE)
-
+    for chamberAndScanDatePair in enumerate(listChamberAndScanDate):
         # Setup the path
         dirPath = getDirByAnaType(options.anaType.strip("Ana"), chamberAndScanDatePair[0], options.ztrim)
         if not filePathExists(dirPath, chamberAndScanDatePair[1]):
