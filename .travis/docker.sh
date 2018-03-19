f3d25163e32f4caec3be3fce452f0de030020093
Jared Sturdy <sturdy@cern.ch>
2018-03-19 16:44:13 +0100

    debugging more streamlined docker

--- .travis/docker.sh
+++ .travis/docker.sh
@@ -23,17 +23,19 @@ then
     if [ "$OS_VERSION" = "6" ]
     then
         echo "Starting SLC6 GEM DAQ custom docker image"
-        sudo docker run -d --user daqbuild --rm=true -v `pwd`:/home/daqbuild/gem-plotting-tools:rw --entrypoint="/bin/bash" \
+        docker run -d --user daqbuild --rm=true -v `pwd`:/home/daqbuild/gem-plotting-tools:rw --entrypoint="/bin/bash" \
              ${DOCKER_IMAGE} -ec "echo Testing build on slc6; echo -ne \"------\nEND gem-plotting-tools TESTS\n\";"
     elif [ "$OS_VERSION" = "7" ]
     then
         echo "Starting CC7 GEM DAQ custom docker image"
         docker run --user daqbuild --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup \
-               -v `pwd`:/home/daqbuild/gem-plotting-tools:rw ${DOCKER_IMAGE} /usr/sbin/init
+               -v `pwd`:/home/daqbuild/gem-plotting-tools:rw ${DOCKER_IMAGE} \
+               /bin/bash -ec "echo Testing build on cc7; echo -ne \"------\nEND gem-plotting-tools TESTS\n\";"
+        # /usr/sbin/init
         docker ps -a
         DOCKER_CONTAINER_ID=$(docker ps | grep ${DOCKER_IMAGE} | awk '{print $1}')
         echo DOCKER_CONTAINER_ID=${DOCKER_CONTAINER_ID}
-        docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -ec "echo Testing build on cc7; echo -ne \"------\nEND gem-plotting-tools TESTS\n\";"
+        # docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -ec "echo Testing build on cc7; echo -ne \"------\nEND gem-plotting-tools TESTS\n\";"
     elif [ "$OS_VERSION" = "8" ]
     then
         echo "Starting CC8 GEM DAQ custom docker image"
@@ -41,7 +43,7 @@ then
 
     DOCKER_CONTAINER_ID=$(docker ps | grep ${DOCKER_IMAGE} | awk '{print $1}')
     echo DOCKER_CONTAINER_ID=${DOCKER_CONTAINER_ID}
-    sudo docker logs $DOCKER_CONTAINER_ID
+    docker logs $DOCKER_CONTAINER_ID
     docker ps -a
 else
     DOCKER_CONTAINER_ID=$(docker ps | grep ${DOCKER_IMAGE} | awk '{print $1}')
