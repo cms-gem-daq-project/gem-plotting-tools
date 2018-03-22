a3bdf37f5583423ab474f8b3ad7f887aee4b6d8e
Brian Dorney <brian.l.dorney@cern.ch>
2018-03-22 13:10:07 +0100

    functionality added to ScanDataFitter to provide output in either charge or DAC units

--- fitting/fitScanData.py
+++ fitting/fitScanData.py
@@ -9,7 +9,14 @@ class DeadChannelFinder(object):
         self.isDead[event.vfatN][event.vfatCH] = False
 
 class ScanDataFitter(DeadChannelFinder):
-    def __init__(self):
+    def __init__(self, calDAC2Q_m=None, calDAC2Q_b=None, isVFAT3=False, isIPulse=False):
+        """
+        calDAC2Q_m - list of slope values for "fC = m * cal_dac + b" equation, ordered by vfat position
+        calDAC2Q_b - as calDAC2Q_m but for intercept b
+        isVFAT3 - if using VFAT3
+        isIPulse - if using current pulse mode of vfat3
+        """
+
         super(ScanDataFitter, self).__init__()
 
         from gempython.utils.nesteddict import nesteddict as ndict
@@ -21,6 +28,18 @@ class ScanDataFitter(DeadChannelFinder):
         self.scanCount  = ndict()
         self.scanFits   = ndict()
 
+        self.isVFAT3    = isVFAT3
+        self.isIPulse   = isIPulse
+        self.dict_calSF = dict((calSF, 0.25*calSF+0.25) for calSF in range(0,4))
+
+        self.calDAC2Q_m = [1.]*24
+        if calDAC2Q_m is not None:
+            self.calDAC2Q_m = calDAC2Q_m
+
+        self.calDAC2Q_b = [0.]*24
+        if calDAC2Q_b is not None:
+            self.calDAC2Q_b = calDAC2Q_b
+
         for vfat in range(0,24):
             self.scanFits[0][vfat] = np.zeros(128)
             self.scanFits[1][vfat] = np.zeros(128)
@@ -30,18 +49,37 @@ class ScanDataFitter(DeadChannelFinder):
             self.scanFits[5][vfat] = np.zeros(128)
             self.scanFits[6][vfat] = np.zeros(128, dtype=bool)
             for ch in range(0,128):
-                self.scanFuncs[vfat][ch] = r.TF1('scurveFit_vfat%i_chan%i'%(vfat,ch),'[3]*TMath::Erf((TMath::Max([2],x)-[0])/(TMath::Sqrt(2)*[1]))+[3]',1,253)
-                self.scanHistos[vfat][ch] = r.TH1D('scurve_vfat%i_chan%i_h'%(vfat,ch),'scurve_vfat%i_chan%i_h'%(vfat,ch),254,0.5,254.5)
                 self.scanCount[vfat][ch] = 0
+                self.scanFuncs[vfat][ch] = r.TF1('scurveFit_vfat%i_chan%i'%(vfat,ch),'[3]*TMath::Erf((TMath::Max([2],x)-[0])/(TMath::Sqrt(2)*[1]))+[3]',
+                        self.calDAC2Q_m[vfat]*1+self.calDAC2Q_b[vfat],self.calDAC2Q_m[vfat]*253+self.calDAC2Q_b[vfat])
+                self.scanHistos[vfat][ch] = r.TH1D('scurve_vfat%i_chan%i_h'%(vfat,ch),'scurve_vfat%i_chan%i_h'%(vfat,ch),
+                        254,self.calDAC2Q_m[vfat]*0.5+self.calDAC2Q_b[vfat],self.calDAC2Q_m[vfat]*254.5+self.calDAC2Q_b[vfat])
 
         self.fitValid = [ np.zeros(128, dtype=bool) for vfat in range(24) ]
 
+        return
+
     def feed(self, event):
         super(ScanDataFitter, self).feed(event)
-        self.scanHistos[event.vfatN][event.vfatCH].Fill(event.vcal,event.Nhits)
-        if(event.vcal > 250):
-            self.scanCount[event.vfatN][event.vfatCH] += event.Nhits
+
+        charge = self.calDAC2Q_m[event.vfatN]*event.vcal+self.calDAC2Q_b[event.vfatN]
+        if self.isVFAT3: #v3 electronics
+            if event.isIPulse:
+                #Q = CAL_DUR * CAL_DAC * 10nA * CAL_FS
+                charge = (1./ 40079000) * event.vcal * (10 * 1e-9) * self.dict_calSF[event.calSF] * 1e15
+                if(event.vcal > 254):
+                    self.scanCount[event.vfatN][event.vfatCH] += event.Nhits
+            else:
+                charge = self.calDAC2Q_m[event.vfatN]*(256-event.vcal)+self.calDAC2Q_b[event.vfatN]
+                if((256-event.vcal) > 254):
+                    self.scanCount[event.vfatN][event.vfatCH] += event.Nhits
+        else:
+            if(event.vcal > 250):
+                self.scanCount[event.vfatN][event.vfatCH] += event.Nhits
+        self.scanHistos[event.vfatN][event.vfatCH].Fill(charge,event.Nhits)
         self.Nev[event.vfatN][event.vfatCH] = event.Nev
+
+        return
 
     def feedHisto(self, vfatN, vfatCH, histo, nEvts=None):
         self.scanHistos[vfatN][vfatCH] = histo
@@ -52,13 +90,32 @@ class ScanDataFitter(DeadChannelFinder):
         else:
             self.Nev[vfatN][vfatCH] = nEvts
 
+        return
+
     def fit(self):
+        """
+        Iteratively fits all scurves
+        Note:   if the user supplied calDAC2Q_m and calDAC2Q_b at
+                construction then output container will have relevant
+                parameters in charge units instead of DAC units
+
+        Returns self.scanFits
+            
+                 [0][vfat][ch] = scurve mean in either DAC units or charge (threshold of comparator)
+                 [1][vfat][ch] = scurve width in either DAC units or charge (noise seen by comparator)
+                 [2][vfat][ch] = scurve pedestal in hits (e.g. at 0 VCAL this is the noise)
+                 [3][vfat][ch] = fitChi2
+                 [4][vfat][ch] = self.scanCount[vfat][ch]
+                 [5][vfat][ch] = fitNDF
+        """
+
         r.gROOT.SetBatch(True)
         r.gStyle.SetOptStat(0)
 
         random = r.TRandom3()
         random.SetSeed(0)
-        fitTF1 = r.TF1('myERF','[3]*TMath::Erf((TMath::Max([2],x)-[0])/(TMath::Sqrt(2)*[1]))+[3]',1,253)
+        fitTF1 = r.TF1('myERF','[3]*TMath::Erf((TMath::Max([2],x)-[0])/(TMath::Sqrt(2)*[1]))+[3]',
+                        self.calDAC2Q_m[vfat]*1+self.calDAC2Q_b[vfat],self.calDAC2Q_m[vfat]*253+self.calDAC2Q_b[vfat])
         for vfat in range(0,24):
             print 'fitting vfat %i'%vfat
             for ch in range(0,128):
@@ -73,16 +130,16 @@ class ScanDataFitter(DeadChannelFinder):
                     rand = random.Gaus(10, 5)
                     if (rand < 0.0 or rand > 100): continue
                     # Provide an initial guess
-                    fitTF1.SetParameter(0, 8+stepN*8)
-                    fitTF1.SetParameter(1,rand)
-                    fitTF1.SetParameter(2,8+stepN*8)
+                    fitTF1.SetParameter(0, self.calDAC2Q_m[vfat]*(8+stepN*8)+self.calDAC2Q_b[vfat] )
+                    fitTF1.SetParameter(1, self.calDAC2Q_m[vfat]*(rand)+self.calDAC2Q_b[vfat])
+                    fitTF1.SetParameter(2, self.calDAC2Q_m[vfat]*(8+stepN*8)+self.calDAC2Q_b[vfat])
                     fitTF1.SetParameter(3, self.Nev[vfat][ch]/2.)
 
                     # Set Parameter Limits
-                    fitTF1.SetParLimits(0, 0.01, 300.0)
-                    fitTF1.SetParLimits(1, 0.0, 100.0)
-                    fitTF1.SetParLimits(2, 0.0, 300.0)
-                    fitTF1.SetParLimits(3, 0.0, self.Nev[vfat][ch] * 2.)
+                    fitTF1.SetParLimits(0, 0.01, self.calDAC2Q_m[vfat]*(300.0)+self.calDAC2Q_b[vfat])
+                    fitTF1.SetParLimits(1, 0.0,  self.calDAC2Q_m[vfat]*(100.0)+self.calDAC2Q_b[vfat])
+                    fitTF1.SetParLimits(2, 0.0,  self.calDAC2Q_m[vfat]*(300.0)+self.calDAC2Q_b[vfat])
+                    fitTF1.SetParLimits(3, 0.0,  self.Nev[vfat][ch] * 2.)
 
                     # Fit
                     fitResult = self.scanHistos[vfat][ch].Fit('myERF','SQ')
@@ -120,6 +177,7 @@ class ScanDataFitter(DeadChannelFinder):
         inF = r.TFile(treeFileName)
         for event in inF.scurveTree :
             self.feed(event)
+        return
 
 def fitScanData(treeFileName):
     fitter = ScanDataFitter()
