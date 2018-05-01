b60df1c92bcf936bc07f80c8f67038b68dea81bb
Jared Sturdy <sturdy@cern.ch>
2018-05-01 09:45:41 +0200

    cleaned up travis.sh, added CI folder to gitignore

--- .ci/travis.sh
+++ .ci/travis.sh
@@ -10,8 +10,9 @@ PY_VER=$2
 DOCKER_IMAGE=$3
 ROOT_VER=$4
 
-DOCKER_CONTAINER_ID=$(docker ps | grep ${DOCKER_IMAGE} | awk '{print $1}')
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -ec "echo running test job;
-   . /home/daqbuild/gem-plotting-tools/.ci/test_on_docker.sh ${OS_VERSION} ${PY_VER} ${ROOT_VER};"
-
-exit 0
+## drive the different options here, passed in from the parent?
+# setBuildEnv.sh -p ${PYTHON_VERSION} -c ${COMPILER_VERSION}
+if [ ! -z ${1+x} ]
+then
+    eval "$1"
+fi
