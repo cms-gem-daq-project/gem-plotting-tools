4ae393d4f7d3442ed0f4c93b2dc8e33fd597baaa
Jared Sturdy <sturdy@cern.ch>
2018-06-21 14:36:17 +0200

    fixing the versioning, running post install scriptlets

--- pkg/postinstallrpm.sh
+++ pkg/postinstallrpm.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-find /usr/lib -type f -name buildMapFiles.py -exec python {} \;
+find %{python2_sitelib} -type f -name buildMapFiles.py -exec python {} \;
+find %{python2_sitelib}/gempython/gemplotting/macros -type f -iname '*.py' -exec chmod +x {} \+
+
+patchfile=/opt/cmsgemos/etc/patches/gemplotting.patch
+if [ -e $patchfile ]
+then
+    cd %{python2_sitelib}
+    patch -p1 < $patchfile
+fi
