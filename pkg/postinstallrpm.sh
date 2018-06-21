aa5824687184fdaf027efae6982dd06d1d5b7077
Jared Sturdy <sturdy@cern.ch>
2018-06-21 16:13:03 +0200

    add post-install scriptlet to apply patches for specific setups

--- pkg/postinstallrpm.sh
+++ pkg/postinstallrpm.sh
@@ -3,9 +3,9 @@
 find %{python2_sitelib} -type f -name buildMapFiles.py -exec python {} \;
 find %{python2_sitelib}/gempython/gemplotting/macros -type f -iname '*.py' -exec chmod +x {} \+
 
-patchfile=/opt/cmsgemos/etc/patches/gemplotting.patch
+patchfile=/opt/cmsgemos/etc/patches/gempython.gemplotting.patch
 if [ -e $patchfile ]
 then
-    cd %{python2_sitelib}
+    cd %{python2_sitelib}/gempython/gemplotting
     patch -p1 < $patchfile
 fi
